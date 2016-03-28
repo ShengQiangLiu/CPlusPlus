39bf9ee60e4d3c02291b73c60d70c96036f5ec71
Sniper <sniper@ZW-LMtekiMac-mini.local>
2016-03-28 23:07:16 +0800

    init

---
+++ C++类和对象/C++类和对象/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C++类和对象
+//
+//  Created by Sniper on 16/3/28.
+//  Copyright © 2016年 ShengQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
